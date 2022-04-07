889c6938d7bf46a8b190e2c6db5585d7f30f25bc
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-04-07 15:18:54 +0100

    Tidy up code

--- source/public/js/Entity.js
+++ source/public/js/Entity.js
@@ -1,6 +1,6 @@
 // Entity class that holds entity data
 
-const DEFAULT_SETTINGS={
+const DEFAULT_SETTINGS = {
     animations: ['bounce', 'rubberBand', 'swing']
 }
 
@@ -9,12 +9,12 @@ export default class Entity {
     imageURL;
     name;
     animations;
-// Constructor taking settings and applying to object
+    // Constructor taking settings and applying to object
     constructor(settings) {
         // Takes the key values pairs that are passed in through the settings parameter, 
         // spreads the settings into an Array and loops through them,
         // assigns the "value" to the equivalent "key" on the entity object(this)
-        Object.entries({...DEFAULT_SETTINGS, ...settings}).forEach(([key, value]) => {
+        Object.entries({ ...DEFAULT_SETTINGS, ...settings }).forEach(([key, value]) => {
             this[key] = value
         })
     }
