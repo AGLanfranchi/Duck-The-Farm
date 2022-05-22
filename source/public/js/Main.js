63f5d75070062cc5db5805e25299a30abd550690
AGLanfranchi <adam.lanfranchi@gmail.com>
2022-05-22 16:38:59 +0100

    Media queries

--- source/public/js/Main.js
+++ source/public/js/Main.js
@@ -5,17 +5,4 @@ const gameEngine = new GameEngine();
 
 // Start the game
 gameEngine.start();
-
-// Add event listener for keyboard
-document.addEventListener('keydown', (event) => {
-    // Chosen keys in array for moving entities
-    var allowedKeys = ['ArrowRight', 'ArrowLeft']
-    if (allowedKeys.includes(event.code)) {
-        // Loads new entity
-        gameEngine.getEntity();
-        // Display entity
-        gameEngine.displayEntity();
-    }
-});     
-
-// Install Chrome and enable developer mode on phone to talk to phone/PC
+     
